3a1e8f7f9cd8035f269a514496ecdcb7c206d1f4
SenorBlub <ThomasVerhappen@Gmail.com>
2024-02-21 10:49:18 +0100

    Added sort method to train

--- CircusTrainFeb2024/Train.cs
+++ CircusTrainFeb2024/Train.cs
@@ -2,9 +2,14 @@
 
 public class Train
 {
-    public List<Cart> carts;
+    public List<Cart>? carts;
 
     public List<IAnimal> animals;
+
+    public Train(List<IAnimal> _animals)
+    {
+        animals = _animals;
+    }
 
     public long CalculatePlacementEfficiency()
     {
@@ -17,5 +22,50 @@ public class Train
         if(totalSize>0)
             return carts.Count/totalSize;
         return 1000000;
+    }
+
+    public void PreSortAnimals()
+    {
+
+        List<IAnimal> carnivores = new List<IAnimal>();
+        List<IAnimal> herbivores = new List<IAnimal>();
+        foreach (IAnimal animal in animals)
+        {
+            if (animal.diet == Diet.Carnivore)
+            {
+                carnivores.Add(animal);
+            }
+            else
+            {
+                herbivores.Add(animal);
+            }
+        }
+
+        carnivores.Sort((animal1, animal2) => animal1.size.CompareTo(animal2.size));
+        herbivores.Sort((animal1, animal2) => animal1.size.CompareTo(animal2.size));
+
+        List<IAnimal> newAnimals = new List<IAnimal>();
+
+        newAnimals.AddRange(carnivores);
+        newAnimals.AddRange(herbivores);
+
+        animals = newAnimals;
+    }
+
+    public void QuickPlace()
+    {
+        this.PreSortAnimals();
+
+        foreach (IAnimal animal in animals)
+        {
+            
+        }
+
+    }
+
+    public void BestPlace()
+    {
+        this.PreSortAnimals();
+
     }
 }
