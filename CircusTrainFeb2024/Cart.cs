88efd8cd60fe4cb930f4fc20e5c07e7313df9337
SenorBlub <ThomasVerhappen@Gmail.com>
2024-02-21 10:23:54 +0100

    Cart.cs changes

--- CircusTrainFeb2024/Cart.cs
+++ CircusTrainFeb2024/Cart.cs
@@ -2,5 +2,10 @@
 
 public class Cart
 {
-    List<IAnimal> animals;
+    List<IAnimal> animals = new List<IAnimal>(10);
+
+    public int RemainingSpace()
+    {
+        return animals.Count - 10;
+    }
 }
